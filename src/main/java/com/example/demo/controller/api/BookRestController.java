0eed5b7b562dd09136b4c96db4e864a0f3696483
nehemiagueldi <gueldinehemia@gmail.com>
2025-01-02 16:40:03 +0700

    Add api
    - Add endpoint for get detail book by id

--- src/main/java/com/example/demo/controller/api/BookRestController.java
+++ src/main/java/com/example/demo/controller/api/BookRestController.java
@@ -5,9 +5,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.model.Book;
 import com.example.demo.repository.BookRepository;
 import com.example.demo.utils.CustomResponse;
 
@@ -25,5 +27,16 @@ public class BookRestController {
   @GetMapping("book")
   public ResponseEntity<Object> getBooks(){
     return CustomResponse.generate(HttpStatus.OK, "Data Berhasil Didapatkan", bookRepository.findAll());
+  }
+
+  @CrossOrigin
+  @GetMapping("book/{id}")
+  public ResponseEntity<Object> getBookById(@PathVariable Integer id) {
+    Book book = bookRepository.findById(id).orElse(null);
+    if (book != null) {
+      return CustomResponse.generate(HttpStatus.OK, "Data Berhasil Didapatkan", book);
+    } else {
+      return CustomResponse.generate(HttpStatus.OK, "Data Tidak Ditemukan");
+    }
   }
 }
